fb9230670a2de7251a895510332ee6aafd594bdf
Michał Filo <s173393@v-ie.uek.krakow.pl>
2014-11-05 10:08:30 +0100

    zabawy z bootstrapem

--- reflection.php
+++ reflection.php
@@ -1,7 +1,7 @@
 <?php
 
 $con=pg_connect("host=sbazy dbname=s173393 user=s173393 password=5aX77G5e");
-$querry="select reflection from PanTadeusz";
+$querry="select reflection from index.htm";
 $querry="insert into PanTadeusz(title,reflection) values('$title','$reflection')";
 $result=pg_exec($con,$querry);
 
