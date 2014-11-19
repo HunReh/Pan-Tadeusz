b3e842a997e9e69729e469d4c4382a4e23840456
Michał Filo <s173393@v-ie.uek.krakow.pl>
2014-11-19 15:47:29 +0100

    zabawy z bootstrapem

--- database.php
+++ database.php
@@ -4,6 +4,28 @@ class Database{
 	private $con;
 	private $query;
 	private $result;
+	private $host="10.254.94.2";	
+	private $user="s173393";
+	private $password="";
+	private $base="s173393";
+	
+	
+	
+	public function _construct($host, $user, $password, $base){
+	
+	$this->host=$host;
+	$this->user=$user;
+	$this->password=$password;
+	$this->base=$base;
+	}
+	
+	public function connect(){
+		
+	}
+	
+	public function execute($query){
+	
+	}
 	
 	
 
